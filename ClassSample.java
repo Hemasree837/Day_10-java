9ee80d98af094169d99a5c09163472379a1de785
Hemasree837 <hemasree837@gmail.com>
2024-12-11 13:39:52 +0530

    Add files via upload

---
+++ ClassSample.java
@@ -0,0 +1,11 @@
+import java.util.*;
+class a{
+    int a=20;
+}
+class ClassSample{
+    public static void main(String args[])
+    {
+        a obj=new a();
+        System.out.println(obj.a);
+    }
+}
