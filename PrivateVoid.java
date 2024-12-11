d7872f27cdc7b2dc73047f42b94c97638b1b56b6
Hemasree837 <hemasree837@gmail.com>
2024-12-11 13:42:09 +0530

    Add files via upload

---
+++ PrivateVoid.java
@@ -0,0 +1,9 @@
+public class PrivateVoid {
+        private static void s()
+        {
+            System.out.println("Zoro");
+        }
+        public static void main(String args[]){
+        s();
+        }
+}
