66269d3b4ae7f7d9e6a094c926fc8ad48df8d8c1
Hemasree837 <hemasree837@gmail.com>
2024-12-11 13:41:50 +0530

    Add files via upload

---
+++ NonVoidPrivate.java
@@ -0,0 +1,11 @@
+public class NonVoidPrivate {
+    public static int i()
+    {
+        return 10;
+    }
+    public static void main(String args[])
+    {
+       int r=i();
+       System.out.println(r);
+    }
+}
