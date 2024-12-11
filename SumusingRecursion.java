92f6de191de0250faa597bdad33ad5125430669a
Hemasree837 <hemasree837@gmail.com>
2024-12-11 13:43:35 +0530

    Add files via upload

---
+++ SumusingRecursion.java
@@ -0,0 +1,13 @@
+public class SumusingRecursion {
+    int sum (int n)
+    {
+      if(n==0) return 0;
+    return n+sum(n-1);
+    }
+    public static void main(String args[])
+    {
+        SumusingRecursion ob= new SumusingRecursion();
+        int res=ob.sum(10);
+        System.out.println(res);
+    }
+}
