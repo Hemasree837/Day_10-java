550b4717ce1cd855f0b57b675cafc77f4c61257c
Hemasree837 <hemasree837@gmail.com>
2024-12-11 13:41:29 +0530

    Add files via upload

---
+++ Nonvoid.java
@@ -0,0 +1,11 @@
+public class Nonvoid {
+    public static String s()
+    {
+        return "OnePiece";
+    }
+    public static void main(String args[])
+    {
+        String res=s();
+        System.out.println(res);
+    }
+}
